8db44e8f2cd45661853933e00c52382d3fc18b8e
Lucia <lucosiar333@gmail.com>
2023-04-03 13:17:44 +0200

    First Commit

---
+++ app/src/main/java/com/example/a222/FormularioMedicacion/FormularioInicioMActivity.java
@@ -0,0 +1,16 @@
+package com.example.a222.FormularioMedicacion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.a222.R;
+
+public class FormularioInicioMActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_inicio_mactivity);
+    }
+}
